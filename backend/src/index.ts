99e432e360524704d3d46bfa7fb70278a750f951
Xaun Lopez <xaunlopez@gmail.com>
2019-08-20 14:48:29 +1000

    Connect PG client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,16 @@
 import { GraphQLServer } from 'graphql-yoga'
 import { prisma } from './generated/prisma-client'
+import { Client } from 'pg'
 import resolvers from './resolvers'
+
+// Connecting to PG client to run SQL directly
+const client = new Client({
+  database: 'prisma',
+  user: 'prisma',
+  password: 'prisma',
+  host: 'localhost',
+  port: 3001
+})
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -10,4 +20,10 @@ const server = new GraphQLServer({
     prisma,
   }),
 })
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+
+const startServer = async () => {
+  await client.connect()
+  server.start(() => console.log(`Server is running on http://localhost:4000`))
+}
+
+startServer()
