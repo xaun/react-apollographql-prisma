f8c47c7aefa0e2d769bcc9397fb712df4a12a9fe
Jake Ranallo <jake.ranallo@gmail.com>
2019-08-22 09:45:47 +1000

    Revert "Add apollo hook examples and error handling"
    This reverts commit 281b20df83880fc40417f26b7e658aa132d37480.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,4 @@
 import Page from '../components/Page'
-import { Signup } from '../components/Signup'
-import { Feed } from '../components/Feed'
 import { Box } from '../components/atoms/Box'
 
 export default () => (
@@ -13,8 +11,7 @@ export default () => (
         1 / 4 // 25% from the next breakpoint and up
       ]}
     >
-      <Signup />
-      <Feed />
+      React App
     </Box>
   </Page>
 )
