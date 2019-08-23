021e76e7d25362c4a60d8c901e6b55432ca29fb3
Jake Ranallo <jake.ranallo@gmail.com>
2019-08-23 10:06:53 +1000

    Move normalize to GlobalStyles, add global theming example

--- frontend/lib/theme.tsx
+++ frontend/lib/theme.tsx
@@ -5,5 +5,8 @@ export default {
     black: '#000',
     white: '#fff'
   },
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512]
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fonts: {
+    primaryFont: 'helvetica'
+  }
 }
